1184cdd4164d7abf2475b31f7b7cb4fe1a00e4f2
unnamed <onlycoding47@gmail.com>
2021-10-08 13:58:01 +0600

    Upgraded database to postgre

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ from flask_login import (
 )
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(
@@ -33,7 +34,7 @@ gravatar = Gravatar(
 )
 
 ##CONNECT TO DB
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
